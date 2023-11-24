cf46c34fe8212023f47764b7f85407ac1c7e6343
Mohamed Nijadi <mohammednijadi47@gmail.com>
2023-11-24 01:26:59 +0100

    Change user agent

--- scraping/scraping/settings.py
+++ scraping/scraping/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "scraping.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "scraping (+http://www.yourdomain.com)"
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
